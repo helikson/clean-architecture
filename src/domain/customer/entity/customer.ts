7b8966cea33b19731ea8e8b748dbbaae5d807871
Helikson Maurilio Borges Rufino <helikson@pop-os.localdomain>
2023-07-03 22:35:50 -0300

    feat: implements yup validator

--- src/domain/customer/entity/customer.ts
+++ src/domain/customer/entity/customer.ts
@@ -1,5 +1,6 @@
 import Entity from "../../@shared/entity/entity.abstract";
 import NotificationError from "../../@shared/notification/notification.error";
+import CustomerValidatorFactory from "../factory/customer.validator.factory";
 import Address from "../value-object/address";
 
 export default class Customer extends Entity {
@@ -29,19 +30,7 @@ export default class Customer extends Entity {
   }
 
   validate() {
-    if (this._id.length === 0) {
-      this.notification.addError({
-        message: "ID is required",
-        context: "Customer",
-      });
-    }
-
-    if (this._name.length === 0) {
-      this.notification.addError({
-        message: "Name is required",
-        context: "Customer",
-      });
-    }
+    CustomerValidatorFactory.create().validate(this);
   }
 
   changeName(name: string) {
